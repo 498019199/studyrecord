5b97a04b6ddc8ebd8026629a2bd6d2e626b2b38f
zbiii <wszhangbei@gmail.com>
2024-01-02 18:29:31 +0800

    node search

--- Leetcode/structure/red_black_map.h
+++ Leetcode/structure/red_black_map.h
@@ -1,27 +1,64 @@
 #pragma once
 #include <cstdint>
 
+template<typename T>
 class smap
 {
     enum class node_type { node_red, node_black};   
 
-    struct map_node
+    struct rbtree_node
     {
-        void* value;
+        T value;
         node_type type;
+        rbtree_node* left;
+        rbtree_node* right;
     };
+    using node_ptr = *rbtree_node;
+public:
+    void add(const T& value)
+    {
+        if (nullptr == head)
+        {
+            head = new_node(value);
+            return ;
+        }
+        
+        
+    }
 
 private:
-    map_node* new_node(void* value)
+    node_ptr find(const T& key)
     {
-        map_node* tmp = new map_node;    
+        node_ptr pnode = head;
+        node_ptr where_node = head;
+        while (pnode)
+        {
+            if (pnode->value < key)
+            {
+                pnode = pnode->right;
+            }
+            else
+            {
+                pnode = pnode->left;
+                where_node = pnode;
+            }
+        }
+
+        return where_node;
+    }
+
+    rbtree_node* new_node(const T& value)
+    {
+        rbtree_node* tmp = new rbtree_node;    
         tmp->type = node_type::node_black;
-        tmp->value = value;
+        tmp->value = std::move(value);
+        tmp->left = nullptr;
+        tmp->right = nullptr;
         return tmp;
     }
     
 private:
-    map_node* head;
+    rbtree_node* head;
     uint32_t size;
     uint32_t capatity;
 };
