c0b05e79e882d08bf96ece776a8b000ae4e95cad
zbiii <wszhangbei@gmail.com>
2024-01-10 17:57:27 +0800

    同步

--- Leetcode/structure/public/include/public/rbtree.h
+++ Leetcode/structure/public/include/public/rbtree.h
@@ -6,7 +6,7 @@ namespace DataBase{
 template<typename T>
 struct rbtree_node
 {
-    using node_ptr = rbtree_node*;
+    using NodePtr = rbtree_node*;
     using value_type = T;
 
     rbtree_node* left;
@@ -25,46 +25,101 @@ template<typename T>
 class rbtree
 {
 protected:
-    enum class node_type { node_red, node_black};  
-    using node_ptr = rbtree_node<T>*;
+    enum node_type { node_red, node_black};  
+    using NodePtr = rbtree_node<T>*;
 
-    static char& Color(node_ptr node) { return node->color; }
-    static node_ptr& Left(node_ptr node) { return node->left; }
-    static node_ptr& Parent(node_ptr node) { return node->parent; }
-    static node_ptr& Right(node_ptr node) { return node->right; }
+    static char& Color(NodePtr node) { return node->color; }
+    static char& IsNil(NodePtr node) { return node->nil; }
+    static NodePtr& Left(NodePtr node) { return node->left; }
+    static NodePtr& Parent(NodePtr node) { return node->parent; }
+    static NodePtr& Right(NodePtr node) { return node->right; }
+    NodePtr& Root() { return Parent(Head_);}
 public:
-    
+    rbtree()
+    {
+        Init();
+    }
+
     void Add(const T& value)
     {
-        node_ptr pnode = head_;
-        node_ptr where_node = head_;
-        bool add_left = false;
-        while (pnode->nil)
+        NodePtr pNode = Root();
+        NodePtr pWherenode = Head_;
+        bool bAddLeft = true;
+        while (!IsNil(pNode))
         {
-            where_node = pnode;
-            add_left = value <= pnode->value;
-            pnode = add_left ? pnode->left : pnode->right;
+            pWherenode = pNode;
+            bAddLeft = value <= pNode->value;
+            pNode = bAddLeft ? pNode->left : pNode->right;
         }
         
-        InsertNode(add_left, where_node, value);
+        InsertNode(bAddLeft, pWherenode, value);
     }
 
 private:
-    void InsertNode(bool add_left, node_ptr where_node, const T& value);
 
-    node_ptr NewNode(const T& value, char crg)
+    void RLeft(NodePtr WhereNode);
+    void RRight(NodePtr WhereNode);
+
+    NodePtr NewNode(const T& value, char crg)
     {
-        node_ptr tmp = new rbtree_node;
-        tmp->type = crg;
+        NodePtr tmp = new rbtree_node<T>;
+        tmp->color = crg;
         tmp->nil = false;
-        tmp->value = std::move(value);
+        tmp->value = value;
         tmp->left = nullptr;
         tmp->parent = nullptr;
         tmp->right = nullptr;
         return tmp;
     }
+
+    void InsertNode(bool bAddLeft, NodePtr pWherenode, const T& value)
+    {
+        NodePtr new_node = NewNode(value, node_type::node_red);
+        Size_++;
+
+        if (pWherenode == Head_)
+        {
+            Root() = new_node;
+            Left(Head_) = new_node, Right(Head_) = new_node;
+        }
+        else if (bAddLeft)
+        {
+            Left(pWherenode) = new_node;
+        }
+        else
+        {
+            Right(pWherenode) = new_node;
+        }
+        
+        // 父节点为黑直接插入子节点
+        for (NodePtr node = new_node; node_type::node_red == Color(Parent(new_node)); )
+        {
+            //叔节点不存在 
+            if (Parent(node) == Left(Parent(Parent(node))))
+            {
+                // 右旋
+            }
+            //叔节点存在
+            else
+            {
+            }
+        }
+        
+        // 根节点必须为黑色
+        Color(Head_) = node_type::node_black;
+        //return ;
+    }
+
+    void Init()
+    {
+        Head_ = NewNode(T(), node_black);
+        IsNil(Head_) = true;
+        Root() = Head_;
+        Left(Head_) = Head_, Right(Head_) = Head_;
+        Size_ = 0;
+    }
 private:
-    node_ptr head_;
-    uint32_t size_;
+    NodePtr Head_ = nullptr;
+    uint32_t Size_ = 0;
 };
 };
