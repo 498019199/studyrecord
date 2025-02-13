9a0468e9db9976499e8e893b49b58513165e7372
zbiii <wszhangbei@gmail.com>
2025-02-13 23:04:05 +0800

    fixed imgui cmake

--- Introduction to DirectX 11 3D Game Engine Programming/Chapter7/main_light.cpp
+++ Introduction to DirectX 11 3D Game Engine Programming/Chapter7/main_light.cpp
@@ -18,7 +18,7 @@ void CreateShere()
     // 创建球体网格数据，levels和slices越大，精度越高。
     auto sphere = new RenderableSphere(1.0f, 20, 20, Color(1.f, 1.f, 1.f, 1.f));
     Context::Instance().WorldInstance().AddRenderable(sphere);
-
+  
 }
 
 void Light()
