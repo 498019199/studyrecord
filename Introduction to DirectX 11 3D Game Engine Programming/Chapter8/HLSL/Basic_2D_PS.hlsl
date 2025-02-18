c9d25477d22e13f2993ad279af0641e5775000d0
zbiii <wszhangbei@gmail.com>
2025-02-18 13:35:01 +0800

    add texture box light mode

--- Introduction to DirectX 11 3D Game Engine Programming/Chapter8/HLSL/Basic_2D_PS.hlsl
+++ Introduction to DirectX 11 3D Game Engine Programming/Chapter8/HLSL/Basic_2D_PS.hlsl
@@ -3,5 +3,33 @@
 // 像素着色器
 float4 PS(VertexOut pIn) : SV_Target
 {
-    return gTex.Sample(gSamLinear, pIn.tex);
+    // 标准化法向量
+    pIn.normalW = normalize(pIn.normalW);
+    // 顶点指向眼睛的向量
+    float3 toEyeW = normalize(gEyePosW - pIn.posW);
+
+    // 初始化为0 
+    float4 ambient, diffuse, spec;
+    float4 A, D, S;
+    ambient = diffuse = spec = A = D = S = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+    ComputeDirectionalLight(gMaterial, gDirLight, pIn.normalW, toEyeW, A, D, S);
+    ambient += A;
+    diffuse += D;
+    spec += S;
+
+    ComputePointLight(gMaterial, gPointLight, pIn.posW, pIn.normalW, toEyeW, A, D, S);
+    ambient += A;
+    diffuse += D;
+    spec += S;
+
+    ComputeSpotLight(gMaterial, gSpotLight, pIn.posW, pIn.normalW, toEyeW, A, D, S);
+    ambient += A;
+    diffuse += D;
+    spec += S;
+
+    float4 texColor = gTex.Sample(gSamLinear, pIn.tex);
+    float4 litColor = texColor * (ambient + diffuse) + spec;
+    litColor.a = texColor.a * gMaterial.Diffuse.a;
+    return litColor;
 }
